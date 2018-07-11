59bfca28b67c221ed8642afcf7e18610ecc3ad0d
WACOMalt <wacomalt@gmail.com>
2018-07-10 21:00:56 -0400

    Should work now :)

--- MoveCacheLocation.bat
+++ MoveCacheLocation.bat
@@ -1,9 +1,15 @@
-D:
-mkdir Games
-cd games
-mkdir VRChat
+@echo off
+echo This script will move your VRChat cache folder to a different 
+echo location and create a symlink to keep the game working.
+echo.
+set /p vrcnew="Enter new location for VRChat cache (ex: D:\Games\VRChat) "
+
+if not exist %vrcnew%\vrchat echo Destination folder doesnt exist, creating it...
+if not exist %vrcnew%\vrchat mkdir %vrcnew%\vrchat
+
 C:
 cd %USERPROFILE%\AppData\LocalLow\VRChat
-xcopy vrchat d:\Games\VRChat\vrchat\ /O /X /E /H /K /Y
+echo moving old cache location to new location
+echo D|xcopy vrchat %vrcnew%\vrchat /O /X /E /H /K /Y
 rd /s /q "%USERPROFILE%\AppData\LocalLow\VRChat\vrchat"
-mklink /D vrchat d:\Games\VRChat\vrchat
+mklink /D vrchat %vrcnew%\vrchat
